4909bce6b6605b1c94a6822e6695d5505584e392
Infernalblight <129289821+Infernalblight@users.noreply.github.com>
2024-11-19 16:36:00 +0100

    Update DolphinInjector.lua

--- DolphinInjector.lua
+++ DolphinInjector.lua
@@ -140,7 +140,7 @@ local function startInjecting()
     local fishButtonClicked = false
     fishButton.MouseButton1Click:Connect(function()
         fishButtonClicked = true
-      loadstring(game:HttpGet("https://raw.githubusercontent.com/NYX0HUB/premium/refs/heads/main/main"))()
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/NYX0HUB/premium/refs/heads/main/mains"))()
     end)
 
     -- Wait for 10 seconds or until "Open Fish" is clicked
