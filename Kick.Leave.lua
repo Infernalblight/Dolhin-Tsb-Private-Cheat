32290271955a1bc3331a72ed2657ac79e18186e5
Infernalblight <129289821+Infernalblight@users.noreply.github.com>
2024-11-22 18:00:52 +0100

    Update Kick.Leave.lua

--- Kick.Leave.lua
+++ Kick.Leave.lua
@@ -1,7 +1,7 @@
 local Players = game:GetService("Players")
 local player = Players.LocalPlayer
 
-local kick = true
+local kick = false
 
 if kick then
     player:Kick("You have been banned for cheating with a 300-dollar paid cheat. You are now banned for 99999 years. Goodbye!")
