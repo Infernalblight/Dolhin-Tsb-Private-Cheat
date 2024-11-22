6242c4b909e9265d39b0fa6ffd942d26f41b204a
Infernalblight <129289821+Infernalblight@users.noreply.github.com>
2024-11-22 17:32:46 +0100

    Update Kick.Leave.lua

--- Kick.Leave.lua
+++ Kick.Leave.lua
@@ -1,5 +1,9 @@
 local Players = game:GetService("Players")
 local player = Players.LocalPlayer
 
--- Leave the game immediately
-player:Kick("Goodbye!")
+-- Set this to true to kick the player
+local kick = false
+
+if kick then
+    player:Kick("You have been banned for cheating with a 300 dollar paid cheat now go die 99999 years ban bye.)
+end
