1e7c0aed80588c84f7bba29e37804e06f6fe7e82
Infernalblight <129289821+Infernalblight@users.noreply.github.com>
2024-11-22 18:19:21 +0100

    Update Kick.Leave.lua

--- Kick.Leave.lua
+++ Kick.Leave.lua
@@ -1,9 +1,9 @@
 local Players = game:GetService("Players")
 local player = Players.LocalPlayer
 
-local kick = false
+local kick = true
 
 if kick then
-    player:Kick("You have been banned for cheating with a 300-dollar paid cheat. You are now banned for 99999 years. Goodbye!")
+    player:Kick("CATCH ME A FISH RN NIGGA OR I WILL END U")
 end
 -- NIGGAS
