6db280c7e50e263d3a58d5a38d29b7bbe3ea69db
Infernalblight <129289821+Infernalblight@users.noreply.github.com>
2024-11-23 16:04:28 +0100

    Update Kick.Leave.lua

--- Kick.Leave.lua
+++ Kick.Leave.lua
@@ -4,6 +4,6 @@ local player = Players.LocalPlayer
 local kick = false
 
 if kick then
-    player:Kick("You've been banned for Reason: Auto Fishing Ban Duration:  9999 Years  Note: Ban bypass attempt detected by Liam. The ban bypass system prevented you from getting banned!!!!!")
+    player:Kick("You've been banned for Reason: Auto Fishing Ban Duration:  9999 Years  Note: Ban bypass by Liam. The ban bypass system prevented you from getting banned!!!!!")
 end
 -- NIGGAS
