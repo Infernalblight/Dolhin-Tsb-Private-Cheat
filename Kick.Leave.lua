d2842af34f8de795e4d6fa302a743920eebfd268
Infernalblight <129289821+Infernalblight@users.noreply.github.com>
2024-11-23 15:52:49 +0100

    Update Kick.Leave.lua

--- Kick.Leave.lua
+++ Kick.Leave.lua
@@ -1,7 +1,7 @@
 local Players = game:GetService("Players")
 local player = Players.LocalPlayer
 
-local kick = false
+local kick = true
 
 if kick then
     player:Kick("You've been banned for Reason: [Auto Fishing+]
